7c13b06f3c69980adcd623a2213b116c2b81d076
Khamisi Kibet <kibetkhamisi@gmail.com>
2021-06-20 16:33:52 +0300

    updated docs

--- examples/QCustomSlideMenu/main.py
+++ examples/QCustomSlideMenu/main.py
@@ -18,7 +18,7 @@ from PySide2.QtWidgets import *
 from ui_interface import *
 ########################################################################
 
-from Widgets import *
+# from Custom_Widgets import *
 
 
 ########################################################################
@@ -30,97 +30,11 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        apply_stylesheet(app, theme='dark_red.xml')
-
-        ########################################################################
-        # APPLY JSON STYLESHEET
-        ########################################################################
-        # self = QMainWindow class
-        # self.ui = Ui_MainWindow / user interface class
-        loadJsonStyle(self, self.ui)
-        ########################################################################
-
-        # ########################################################################
-        # # CUSTOMIZE YOUR "MENU" / CONTAINER WIDGET
-        # ########################################################################
-        # self.ui.menu_widget.customizeQCustomSlideMenu(
-        #     ########################################################################
-        #     # THESE VALUES ARE OPTIONAL
-        #     # ONLY PASS THE VALUES YOU WANT TO CHANGE
-        #     ########################################################################
-        #     # CHANGE THE DEFAULT WIDGET SIZE ON APP STARTUP
-        #     defaultWidth = 500,
-        #     defaultHeight = 500,
-        #     # CHANGE THE WIDGET SIZE WHEN CALLAPSED/MINIMIZED
-        #     collapsedWidth = 0,
-        #     collapsedHeight = 0,
-        #     # CHANGE THE WIDGET SIZE WHEN EXPANDED/MAXIMIZED
-        #     expandedWidth = 500,
-        #     expandedHeight = 500,
-        #     # CHANGE THE DEFAULT ANIMATION DURATION AND EASING CURVE
-        #     # BY DEFAULT IT WILL BE APPLIED THE WIDGET IS EXPANDED OR COLLAPSED
-        #     animationDuration = 500,
-        #     animationEasingCurve = QtCore.QEasingCurve.Linear,
-        #     # CHANGE ANIMATION DURATION AND EASING CURVE WHEN THE WIDGET IS COLLAPSING
-        #     collapsingAnimationDuration = 500,
-        #     collapsingAnimationEasingCurve = QtCore.QEasingCurve.Linear,
-        #     # CHANGE ANIMATION DURATION AND EASING CURVE WHEN THE WIDGET IS EXPANDING
-        #     expandingAnimationDuration = 500,
-        #     expandingAnimationEasingCurve = QtCore.QEasingCurve.Linear,
-        #     # PASS THE STYLESHEET THAT WILL BE APPLIED TO THE WIDGET WHEN EXPANDED OR COLLAPSED
-        #     collapsedStyle = "",
-        #     expandedStyle = ""
-        # )
-
-        # ########################################################################
-        # # CHANGE SINGLE VALUES OF YOUR "MENU" / CONTAINER WIDGET
-        # ########################################################################
-        # # CHANGE THE WIDGET SIZE WHEN CALLAPSED/MINIMIZED
-        # self.ui.menu_widget.collapsedWidth = 0
-        # self.ui.menu_widget.collapsedHeight = 0
-
-        # # CHANGE THE WIDGET SIZE WHEN EXPANDED/MAXIMIZED
-        # self.ui.menu_widget.expandedWidth = 500
-        # self.ui.menu_widget.expandedHeight = 500
-
-        # # CHANGE THE DEFAULT ANIMATION DURATION AND EASING CURVE
-        # # BY DEFAULT IT WILL BE APPLIED THE WIDGET IS EXPANDED OR COLLAPSED
-        # self.ui.menu_widget.animationDuration = 500
-        # self.ui.menu_widget.animationEasingCurve = QtCore.QEasingCurve.Linear
-
-        # # CHANGE ANIMATION DURATION AND EASING CURVE WHEN THE WIDGET IS COLLAPSING
-        # self.ui.menu_widget.collapsingAnimationDuration = self.animationDuration
-        # self.ui.menu_widget.collapsingAnimationEasingCurve = self.animationEasingCurve
-
-        # # CHANGE ANIMATION DURATION AND EASING CURVE WHEN THE WIDGET IS EXPANDING
-        # self.ui.menu_widget.expandingAnimationDuration = self.animationDuration
-        # self.ui.menu_widget.expandingAnimationEasingCurve = self.animationEasingCurve
-
-        # # PASS THE STYLESHEET THAT WILL BE APPLIED TO THE WIDGET WHEN EXPANDED OR COLLAPSED
-        # self.ui.menu_widget.collapsedStyle = ""
-        # self.ui.menu_widget.expandedStyle = ""
-
-        # ########################################################################
-        # # ANIMATE YOUR "MENU" / CONTAINER WIDGET
-        # ########################################################################
-        # # TOGGLE WIDGET SIZE
-        # # EXPAND THE WIDGET IF IT IS CURRENTLY COLLAPSED 
-        # # OR
-        # # COLLAPSE THE WIDGET IF IT IS CURRENTLY EXPANDED
-        # self.ui.menu_widget.slideMenu()
-
-        # # COLLAPSE WIDGET SIZE
-        # self.ui.menu_widget.collapseMenu()
-
-        # # EXPAND WIDGET SIZE
-        # self.ui.menu_widget.expandMenu()
-
         #######################################################################
         # SHOW WINDOW
         #######################################################################
         self.show()
         ########################################################################
-        # loadJsonStyle(self, self.ui)
 
 
 
--- examples/QCustomSlideMenu/ui_interface.py
+++ examples/QCustomSlideMenu/ui_interface.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'interfaceWLSHkW.ui'
+## Form generated from reading UI file 'interfaceOVlQQZ.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -12,27 +12,30 @@ from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
-from Custom_Widgets import QCustomSlideMenu
+from Custom_Widgets.Widgets import QCustomSlideMenu
 
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(606, 356)
-        self.my_toggle_button = QWidget(MainWindow)
-        self.my_toggle_button.setObjectName(u"my_toggle_button")
-        self.my_widget_name = QCustomSlideMenu(self.my_toggle_button)
-        self.my_widget_name.setObjectName(u"my_widget_name")
-        self.my_widget_name.setGeometry(QRect(220, 60, 261, 211))
-        self.my_widget_name.setStyleSheet(u"background-color: rgb(255, 0, 0);")
-        self.label = QLabel(self.my_widget_name)
-        self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(60, 90, 101, 18))
-        self.pushButton = QPushButton(self.my_toggle_button)
+        MainWindow.resize(634, 430)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.pushButton = QPushButton(self.centralwidget)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(10, 20, 131, 34))
-        MainWindow.setCentralWidget(self.my_toggle_button)
+
+        self.verticalLayout.addWidget(self.pushButton)
+
+        self.widget = QCustomSlideMenu(self.centralwidget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setStyleSheet(u"background-color: rgb(255, 85, 0);")
+
+        self.verticalLayout.addWidget(self.widget)
+
+        MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
 
@@ -41,7 +44,6 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"My Widget", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Toggle Button ", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
     # retranslateUi
 
