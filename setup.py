fb2ba447f45a47a6ed2011f3ac78f46691ae1018
Khamisi Kibet <82152373+KhamisiKibet@users.noreply.github.com>
2023-06-28 04:38:56 -0700

    Released v 0.6.7

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'Custom_Widgets.Qss',
     ],
     include_package_data=True,
-    version = '0.6.4',      # Start with a small number and increase it with every change you make
+    version = '0.6.7',      # Start with a small number and increase it with every change you make
     license="GNU General Public License v3.0",        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description = 'Custom widgets and widget animations made for QT applications',   # Give a short description about your library
     long_description=README,
@@ -25,7 +25,7 @@ setup(
     author_email = 'kibetkhamisi@gmail.com',      # Type in your E-Mail
     url = 'https://github.com/KhamisiKibet/QT-PyQt-PySide-Custom-Widgets',   # Provide either the link to your github or to your website
     # download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-    keywords = ['PySide', 'PyQt', 'animation', 'custom', 'widgets', "QML", "C++", "QT Creator"],   # Keywords that define your package best
+    keywords = ['PySide', 'PyQt', 'animation', 'custom', 'widgets', "QML", "C++", "QT Creator", "Moder GUI", "Desktop GUI", "Design"],   # Keywords that define your package best
     install_requires=[
         # "PySide2",
         # "PyQt5",
